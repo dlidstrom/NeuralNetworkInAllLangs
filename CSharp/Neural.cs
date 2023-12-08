e9fc3066023a8bb6b0a327f42c0cfec8b1ce67f2
Daniel Lidstrom <dlidstrom@gmail.com>
2023-12-08 08:43:33 +0100

    fix C#

--- CSharp/Neural.cs
+++ CSharp/Neural.cs
@@ -33,11 +33,12 @@ public static class ActivationFunctions
 }
 
 public record Network(
+    int InputCount,
     int HiddenCount,
     int OutputCount,
-    double[][] WeightsHidden,
+    double[] WeightsHidden,
     double[] BiasesHidden,
-    double[][] WeightsOutput,
+    double[] WeightsOutput,
     double[] BiasesOutput)
 {
     public double[] Predict(double[] input)
@@ -49,22 +50,23 @@ public record Network(
 
     public double[] Predict(double[] input, double[] y_hidden, double[] y_output)
     {
-        for (int c = 0; c < WeightsHidden[0].Length; c++)
+        for (int c = 0; c < HiddenCount; c++)
         {
             double sum = 0.0;
-            for (int r = 0; r < WeightsHidden.Length; r++)
+            for (int r = 0; r < InputCount; r++)
             {
-                sum += input[r] * WeightsHidden[r][c];
-                y_hidden[c] = ActivationFunctions.Sigmoid(sum + BiasesHidden[c]);
+                sum += input[r] * WeightsHidden[r * HiddenCount + c];
             }
+
+            y_hidden[c] = ActivationFunctions.Sigmoid(sum + BiasesHidden[c]);
         }
 
-        for (int c = 0; c < WeightsOutput[0].Length; c++)
+        for (int c = 0; c < OutputCount; c++)
         {
             double sum = 0.0;
-            for (int r = 0; r < WeightsOutput.Length; r++)
+            for (int r = 0; r < HiddenCount; r++)
             {
-                sum += y_hidden[r] * WeightsOutput[r][c];
+                sum += y_hidden[r] * WeightsOutput[r * OutputCount + c];
             }
 
             y_output[c] = ActivationFunctions.Sigmoid(sum + BiasesOutput[c]);
@@ -96,52 +98,46 @@ public record Trainer(
         double[] output = new double[outputCount];
         double[] gradHidden = new double[hiddenCount];
         double[] gradOutput = new double[outputCount];
-        double[][] weightsHidden = Enumerable
-            .Range(1, inputCount)
-            .Select(_ => Array.ConvertAll(new double[hiddenCount], v => rand.Invoke() - 0.5))
-            .ToArray();
+        double[] weightsHidden = Array.ConvertAll(new double[inputCount * hiddenCount], v => rand.Invoke() - 0.5);
         double[] biasesHidden = new double[hiddenCount];
-        double[][] weightsOutput = Enumerable
-            .Range(1, hiddenCount)
-            .Select(_ => Array.ConvertAll(new double[outputCount], v => rand.Invoke() - 0.5))
-            .ToArray();
+        double[] weightsOutput = Array.ConvertAll(new double[hiddenCount * outputCount], v => rand.Invoke() - 0.5);
         double[] biasesOutput = new double[outputCount];
-        Network network = new(hiddenCount, outputCount, weightsHidden, biasesHidden, weightsOutput, biasesOutput);
+        Network network = new(inputCount, hiddenCount, outputCount, weightsHidden, biasesHidden, weightsOutput, biasesOutput);
         return new Trainer(network, hidden, output, gradHidden, gradOutput);
     }
 
     public void Train (double[] input, double[] y, double lr)
     {
         _ = Network.Predict(input, Hidden, Output);
-        for (int c = 0; c < Output.Length; c++)
+        for (int c = 0; c < Network.OutputCount; c++)
         {
             GradOutput[c] = (Output[c] - y[c]) * ActivationFunctions.SigmoidPrim(Output[c]);
         }
 
-        for (int r = 0; r < Network.WeightsOutput.Length; r++)
+        for (int r = 0; r < Network.HiddenCount; r++)
         {
             double sum = 0.0;
-            for (int c = 0; c < Network.WeightsOutput[0].Length; c++)
+            for (int c = 0; c < Network.OutputCount; c++)
             {
-                sum += GradOutput[c] * Network.WeightsOutput[r][c];
+                sum += GradOutput[c] * Network.WeightsOutput[r * Network.OutputCount + c];
             }
 
             GradHidden[r] = sum * ActivationFunctions.SigmoidPrim(Hidden[r]);
         }
 
-        for (int r = 0; r < Network.WeightsOutput.Length; r++)
+        for (int r = 0; r < Network.HiddenCount; r++)
         {
-            for (int c = 0; c < Network.WeightsOutput[0].Length; c++)
+            for (int c = 0; c < Network.OutputCount; c++)
             {
-                Network.WeightsOutput[r][c] -= lr * GradOutput[c] * Hidden[r];
+                Network.WeightsOutput[r * Network.OutputCount + c] -= lr * GradOutput[c] * Hidden[r];
             }
         }
 
-        for (int r = 0; r < Network.WeightsHidden.Length; r++)
+        for (int r = 0; r < Network.InputCount; r++)
         {
-            for (int c = 0; c < Network.WeightsHidden[0].Length; c++)
+            for (int c = 0; c < Network.HiddenCount; c++)
             {
-                Network.WeightsHidden[r][c] -= lr * GradHidden[c] * input[r];
+                Network.WeightsHidden[r * Network.HiddenCount + c] -= lr * GradHidden[c] * input[r];
             }
         }
 
